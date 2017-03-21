505eb2b23ae8c68c6c5893a7e0a5de96e6a67792
zhanghTK <510223064@qq.com>
2017-03-21 23:47:35 +0800

    feat 添加数据访问异常基础类
    添加数据访问异常基础类

---
+++ src/main/java/tk/zhangh/scalpel/jdbc/exception/DataAccessException.java
@@ -0,0 +1,16 @@
+package tk.zhangh.scalpel.jdbc.exception;
+
+/**
+ * 数据访问异常基础类
+ * Created by ZhangHao on 17/3/21.
+ */
+public class DataAccessException extends RuntimeException {
+
+    public DataAccessException(String msg) {
+        super(msg);
+    }
+
+    public DataAccessException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
