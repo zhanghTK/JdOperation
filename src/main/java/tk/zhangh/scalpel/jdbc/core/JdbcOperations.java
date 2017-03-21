14fbf7555d3cf9419e341ac0f5c4c3e4fe51d4fe
zhanghTK <510223064@qq.com>
2017-03-22 00:37:56 +0800

    feat 添加JdbcOperations
    定义JdbcTemplate操作

---
+++ src/main/java/tk/zhangh/scalpel/jdbc/core/JdbcOperations.java
@@ -0,0 +1,15 @@
+package tk.zhangh.scalpel.jdbc.core;
+
+/**
+ * 定义JdbcTemplate操作
+ * Created by ZhangHao on 17/3/21.
+ */
+public interface JdbcOperations {
+    <R> R execute(ConnectionCallback<R> action);
+
+    <R> R execute(StatementCallback<R> action);
+
+    <R> R execute(String sql, PreparedStatementCallback<R> action);
+
+    <R> R execute(String sql);
+}
